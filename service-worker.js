912f3d8fbac0dec8d1b80e945063484c09f8138a
Stoyan Koychev <stoqnkoi4ev@abv.bg>
2018-05-26 20:08:33 +0300

    Fix: Fixsing the Service Worker

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,34 @@
-let cacheName = 'app-v1';
+self.addEventListener('install', event => {
+  console.log('[SW] - Installed');
+  event.waitUntil(
+    caches.open( 'app-v1' ).then( cache => {
+      return cache.addAll( [
+        './',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './data/restaurants.json',
+        './css/styles.css',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg'
+      ] );
+    }));
+});
 
-self.addEventListener('install', function( e ) {
-  console.log('[ServiceWorker] instaled');
-  e.waitUntil(
-    caches.open( cacheName ).then( function( cache ) {
-      console.log('[ServiceWorker] caches.open '+ cacheName);
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'data/restaurants.json',
-        'css/styles.css',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg'
-      ]);
+self.addEventListener('fetch', event => {
+  console.log('[SW] - Fetched');
+  event.respondWith(
+    caches.match( event.request ).then( response => {
+      if ( response )return response;
+      return fetch( event.request );
     })
   );
-});
-
-
-self.addEventListener('activate', function( event ) {
-  console.log('[ServiceWorker] activated');
-  event.waitUntil(
-    caches.keys().then( function(cacheNames) {
-      return Promise.all(cacheNames.map(function(thisCacheName) {
-        if (thisCacheName !== cacheName) {
-          console.log('[ServiceWorker] Removing Cached Files From', thisCacheName);
-          return caches.delete(thisCacheName);
-        }
-      }))
-    })
-  )
-});
-
-self.addEventListener('fetch', function( event ) {
-  console.log('[ServiceWorker] fetched');
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request)
-        .then( function(res) {
-          if (!res || res.status !== 200|| res.type !== 'basic') {
-            return res;
-          }
-
-          let resp = res.clone();
-          caches.open(cacheName).then( function(cache) {
-              cache.put(event.request, resp);
-            });
-          return res;
-
-        })
-    })
-  )
 });
